a0f1e75d261e2d490c56e230562535f6d64cdfcd
J.D. Sullivan <joe_sullivan1@yahoo.com>
2025-05-26 21:48:08 -0500

    chore: increment iOS build number to 2

--- app.config.ts
+++ app.config.ts
@@ -19,6 +19,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     bundleIdentifier: 'media.hilo.pact',
     supportsTablet: false,
+    buildNumber: '2',
     infoPlist: {
       UIBackgroundModes: ['remote-notification']
     }
